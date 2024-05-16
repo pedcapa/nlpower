666647bebef2452124e4659a748d5b7d6eeb9f58
Pedro Castañeda <113157404+catbox-dev@users.noreply.github.com>
2024-05-15 20:00:41 -0600

    Reformat file

--- app/services/text_processing.py
+++ app/services/text_processing.py
@@ -9,15 +9,18 @@ nltk.download('vader_lexicon')
 nlp = spacy.load("en_core_web_sm")
 stop_words = set(stopwords.words('english') + list(punctuation))
 
+
 def analyze_sentiment(text):
     sid = SentimentIntensityAnalyzer()
     score = sid.polarity_scores(text)
     return "Positive" if score['compound'] >= 0.05 else ("Negative" if score['compound'] <= -0.05 else "Neutral")
 
+
 def extract_keywords(text):
     words = nltk.word_tokenize(text)
     keywords = [word for word in words if word.lower() not in stop_words]
     return ', '.join(set(keywords))
+
 
 def summarize_text(text):
     doc = nlp(text)
